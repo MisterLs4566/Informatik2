45856d1e90e70ee81447d7bd2285027e2f66d6a7
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2022-01-02 00:31:54 +0100

    version 2.4

---
+++ scripts/overview.js
@@ -0,0 +1,44 @@
+function assembler()
+{
+    window.open("../sites/syntax.html", "_self");
+}
+
+function basic()
+{
+    window.open("../sites/syntax.html", "_self");
+}
+
+function pascal()
+{
+    window.open("../sites/syntax.html", "_self");
+}
+
+function c()
+{
+    window.open("../sites/syntax.html", "_self");
+}
+
+function cpp()
+{
+    window.open("../sites/syntax.html", "_self");
+}
+
+function python()
+{
+    window.open("../sites/syntax.html", "_self");
+}
+
+function java()
+{
+    window.open("../sites/syntax.html", "_self");
+}
+
+function javascript()
+{
+    window.open("../sites/syntax.html", "_self");
+}
+
+function php()
+{
+    window.open("../sites/syntax.html", "_self");
+}
---
+++ scripts/syntax.js
@@ -0,0 +1,44 @@
+function assembler()
+{
+    window.open("../sites/overview.html", "_self");
+}
+
+function basic()
+{
+    window.open("../sites/overview.html", "_self");
+}
+
+function pascal()
+{
+    window.open("../sites/overview.html", "_self");
+}
+
+function c()
+{
+    window.open("../sites/overview.html", "_self");
+}
+
+function cpp()
+{
+    window.open("../sites/overview.html", "_self");
+}
+
+function python()
+{
+    window.open("../sites/overview.html", "_self");
+}
+
+function java()
+{
+    window.open("../sites/overview.html", "_self");
+}
+
+function javascript()
+{
+    window.open("../sites/overview.html", "_self");
+}
+
+function php()
+{
+    window.open("../sites/overview.html", "_self");
+}
