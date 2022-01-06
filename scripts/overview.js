9dda90f2bd885f5497192f275b812326afaa348e
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2022-01-06 03:17:24 +0100

    version 3.1

--- scripts/overview.js
+++ scripts/overview.js
@@ -5,40 +5,40 @@ function assembler()
 
 function basic()
 {
-    window.open("../sites/syntax.html", "_self");
+    window.open("../sites/syntax.html#basic", "_self");
 }
 
 function pascal()
 {
-    window.open("../sites/syntax.html", "_self");
+    window.open("../sites/syntax.html#pascal", "_self");
 }
 
 function c()
 {
-    window.open("../sites/syntax.html", "_self");
+    window.open("../sites/syntax.html#c", "_self");
 }
 
 function cpp()
 {
-    window.open("../sites/syntax.html", "_self");
+    window.open("../sites/syntax.html#c++", "_self");
 }
 
 function python()
 {
-    window.open("../sites/syntax.html", "_self");
+    window.open("../sites/syntax.html#python", "_self");
 }
 
 function java()
 {
-    window.open("../sites/syntax.html", "_self");
+    window.open("../sites/syntax.html#java", "_self");
 }
 
 function javascript()
 {
-    window.open("../sites/syntax.html", "_self");
+    window.open("../sites/syntax.html#javascript", "_self");
 }
 
 function php()
 {
-    window.open("../sites/syntax.html", "_self");
+    window.open("../sites/syntax.html#php", "_self");
 }
--- scripts/syntax.js
+++ scripts/syntax.js
@@ -1,44 +1,39 @@
-function assembler()
-{
-    window.open("../sites/overview.html", "_self");
-}
-
 function basic()
 {
-    window.open("../sites/overview.html", "_self");
+    window.open("../sites/overview.html#basic", "_self");
 }
 
 function pascal()
 {
-    window.open("../sites/overview.html", "_self");
+    window.open("../sites/overview.html#pascal", "_self");
 }
 
 function c()
 {
-    window.open("../sites/overview.html", "_self");
+    window.open("../sites/overview.html#c", "_self");
 }
 
 function cpp()
 {
-    window.open("../sites/overview.html", "_self");
+    window.open("../sites/overview.html#c++", "_self");
 }
 
 function python()
 {
-    window.open("../sites/overview.html", "_self");
+    window.open("../sites/overview.html#python", "_self");
 }
 
 function java()
 {
-    window.open("../sites/overview.html", "_self");
+    window.open("../sites/overview.html#java", "_self");
 }
 
 function javascript()
 {
-    window.open("../sites/overview.html", "_self");
+    window.open("../sites/overview.html#javascript", "_self");
 }
 
 function php()
 {
-    window.open("../sites/overview.html", "_self");
+    window.open("../sites/overview.html#php", "_self");
 }
